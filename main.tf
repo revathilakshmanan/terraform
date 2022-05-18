653e39671c8fe2aa607ede70d3826036227a3a5c
revathilakshmanan <77661388+revathilakshmanan@users.noreply.github.com>
2022-05-18 12:22:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
-test
+provider "aws" {
+    region = "us-west-2"
+    access_key = "AKIAWI5PPWEPRABSOYGU"
+    secret_key = "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"
+}
+
+resource "aws_instance" "ubuntu" {
+    ami = "ami-0ee8244746ec5d6d4"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "revathi"
+        Provider = "terraform"
+    } 
+}
+
+
+
