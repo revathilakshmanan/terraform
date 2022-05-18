4a73b29ac92d4582c7dd1bf9cd0c2d05ac3b845d
revathilakshmanan <77661388+revathilakshmanan@users.noreply.github.com>
2022-05-18 14:57:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 provider "aws" {
     region = "us-west-2"
-    access_key = ""
-    secret_key = ""
+    access_key = "AKIAYITFZ72IT3LSGKAK"
+    secret_key = "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"
 }
+
+variable public_key_location{}
+
 
 resource "aws_instance" "ubuntu" {
     ami = "ami-0ee8244746ec5d6d4"
@@ -11,8 +14,15 @@ resource "aws_instance" "ubuntu" {
     tags = {
         Name = "revathi"
         Provider = "terraform"
-    } 
+    }
 }
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key1"
+  public_key =""  
+ # public_key = file("<var.public_key_location>")
+}
+
 
 
 
