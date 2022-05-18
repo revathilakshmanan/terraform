4b6f9ded5d30d03b4a3b5a552df45b7d0c138b07
revathilakshmanan <77661388+revathilakshmanan@users.noreply.github.com>
2022-05-18 12:36:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-west-2"
-    access_key = "AKIAWI5PPWEPRABSOYGU"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 resource "aws_instance" "ubuntu" {
